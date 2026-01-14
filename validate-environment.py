b3f6530b6d032cea127ea363c389bb48cac2f4e4
ShaneJobes <136496607+ShaneJobes@users.noreply.github.com>
2026-01-14 17:50:48 +0000

    started validation test code, follow the rest of the week1 workflow bootcamp file in module1 folder

---
+++ validate-environment.py
@@ -0,0 +1,10 @@
+import sys 
+import platform
+from datetime import datetime
+def validate_environment():
+    """Run basic environment checks and report status."""
+
+    print ("=" * 50)
+    print ("ALGOCRATIC FUTURES™ ENVIRONMENT VALIDATION")
+    print ("=" * 50)
+    print ()
